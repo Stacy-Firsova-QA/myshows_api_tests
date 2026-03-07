9e44e8da5f3cbfe37da6f147935a84c272e45d4b
Anastasia <anpogodina2001@yandex.ru>
2026-03-07 19:32:53 +0300

    Revert "add files test_myshows_api.py and others for project"
    This reverts commit 88282025f6be8c530b5f41e4b2aa7b2a2e8ade5f.

--- config/db_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DB_CONN_PARAMS = {
-        "dbname": "my-shows-rating",
-        "user": "postgres",
-        "password": os.getenv("DB_PASSWORD"),
-        "host": "127.0.0.1",
-        "port": "5432"
-    }
--- helpers/file_helpers.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-import yaml
-
-def load_yaml(filename:str) -> dict:
-    path = Path(__file__).parent.parent / "schemas" / filename
-    with path.open(mode="r", encoding="utf-8") as file:
-        return yaml.safe_load(file)
